60f6b7ae54e90396c671b145cce07b84cb52f76d
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-09-01 19:25:48 +0200

    Grid Component

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,19 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
-    </v-main>
+    <v-content>
+      <grid />
+    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Grid from "./components/Grid";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
+    Grid
   },
 
   data: () => ({
@@ -54,3 +21,8 @@ export default {
   })
 };
 </script>
+<style>
+#app {
+  background: #232222;
+}
+</style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-row v-for="(row, rowIdx) in 8" :key="rowIdx" justify="center" no-gutters>
+      <template v-for="(col, colIdx) in 8">
+        <v-hover v-slot:default="{ hover }" :key="colIdx">
+          <v-col>
+            <v-card :class="[{ 'on-hover': hover }, 'grid-cell', 'flex-center']" elevation="6" dark></v-card>
+          </v-col>
+        </v-hover>
+      </template>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.grid-cell {
+  height: 2rem;
+  width: 2rem;
+}
+.flex-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.col {
+  flex-grow: 0;
+}
+.on-hover {
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0.01) !important;
+}
+</style>
